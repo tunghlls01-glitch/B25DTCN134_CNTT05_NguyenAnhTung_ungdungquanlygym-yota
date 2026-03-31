8505bca081d514ebb94f31aeecf0e44d4ef5f692
tunghlls01-glitch <tunghlls01@gmail.com>
2026-04-01 00:35:53 +0700

    Add files via upload

--- Project IT103-b/js/register.js
+++ Project IT103-b/js/register.js
@@ -6,14 +6,14 @@ let error_confirmPassword = document.getElementById("error_confirmPassword");
 // lấy dữ liệu localStorages
 let users = JSON.parse(localStorage.getItem("users")) || [];
 //hàm validate trống
-function empty_condition (value,errorElement, name) {
+function empty_condition(value, errorElement, name) {
     if (value.trim() === "") {
         errorElement.innerText = name + " không được để trống"
         return false;
     }
     errorElement.innerText = "";
     errorElement.style.display = "none";
-    return true; 
+    return true;
 }
 //hàm định dạng email 
 function validateEmail(email) {
@@ -21,13 +21,25 @@ function validateEmail(email) {
     return regex.test(email);
 }
 //đối tượng admin
-let admin = {
-    id: Math.ceil(Math.random() * 10000000000000),
-    name: "Nguyễn Anh Tùng",
-    email: "anhtungls01@gmail.com",
-    role: "admin",
-    password: "anhtung01"
-}
+let admins = [
+    {
+        id: Math.ceil(Math.random() * 10000000000000),
+        name: "Nguyễn Anh Tùng",
+        email: "anhtungls01@gmail.com",
+        role: "admin",
+        password: "anhtung01"
+    },
+    {
+        id: Math.ceil(Math.random() * 10000000000000),
+        name: "Nguyễn Tùng",
+        email: "nguyentungls01@gmail.com",
+        role: "admin",
+        password: "nguyentung01"
+    }
+];
+//lấy đối tượng addmin
+localStorage.setItem("admins", JSON.stringify(admins));
+//hàm check điều kiện 
 function addlogin() {
     //lấy
     let name = document.getElementById("userName").value;
@@ -35,16 +47,16 @@ function addlogin() {
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
     let check = true;
-    if (!empty_condition (name,error_name, "họ và tên")) {
+    if (!empty_condition(name, error_name, "họ và tên")) {
         check = false;
     }
-    if (!empty_condition (email,error_email, "Email")) {
+    if (!empty_condition(email, error_email, "Email")) {
         check = false;
-    } else if(users.some(value => value.email === email)) {
+    } else if (users.some(value => value.email === email)) {
         error_email.innerText = "Tồn tại email";
         error_email.style.display = "block"
         check = false;
-    } else if(!validateEmail(email)) {
+    } else if (!validateEmail(email)) {
         error_email.innerText = "Email không đúng định dạng";
         error_email.style.display = "block"
         check = false;
@@ -52,7 +64,7 @@ function addlogin() {
         error_email.innerText = "";
         error_email.style.display = "none";
     }
-    if (!empty_condition (password,error_password, "Mật khẩu")) {
+    if (!empty_condition(password, error_password, "Mật khẩu")) {
         check = false;
     } else if (password.length < 8) {
         error_password.innerText = "password phải lớn hơn 8 kí tự";
@@ -62,9 +74,9 @@ function addlogin() {
         error_password.innerText = "";
         error_password.style.display = "none";
     }
-    if (!empty_condition (confirmPassword,error_confirmPassword,"Xác nhận mật khẩu")) {
+    if (!empty_condition(confirmPassword, error_confirmPassword, "Xác nhận mật khẩu")) {
         check = false;
-    }  else if (password !== confirmPassword) {
+    } else if (password !== confirmPassword) {
         error_confirmPassword.innerText = "Mật khẩu không trùng khớp";
         error_confirmPassword.style.display = "block"
         check = false;
@@ -82,12 +94,12 @@ function addlogin() {
         };
         users.push(user);
         localStorage.setItem("users", JSON.stringify(users));
-        setTimeout(function(){
+        setTimeout(function () {
             document.getElementById("status").style.display = "block";
         }, 1);
-        setTimeout(function(){
+        setTimeout(function () {
             window.location.href = "login.html";
-        }, 1000);   
+        }, 1000);
     }
 }
 
